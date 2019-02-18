1854b8c5dc56dcd5ce0bbdb494058fb6c9bd51bb
Aaron Chapman <aaronchapman95@gmail.com>
2019-02-17 20:21:00 -0500

    set up extension
    prolly shoulda committed more before building the entire extension lmao

---
+++ extension/stocka.js
@@ -0,0 +1,84 @@
+// sort tickers alphabetically
+// list of predefined screener links
+
+
+let tickers = ['AEYE', 'CRBP', 'DTRM', 'HARP', 'USAT'];
+
+function stock_up() {
+	let url = 'https://api.iextrading.com/1.0/stock/market/batch?types=quote&symbols=' + tickers.join(',') + '&filter=symbol,change,latestPrice';
+	let info = {};
+  
+  fetch(url).then(res => res.json()).then(data => set_content(data));
+}
+
+function set_content(data) {
+	$('.ticker_list').empty();
+	$('.tickers_to_add').val('');
+	
+	tickers.forEach(function(ticker) {
+		var markup = '<div class="ticker_container">';
+		markup += '<button class="remove_ticker">✕</button>';
+		markup += '<span class="ticker">' + ticker + ': $' + data[ticker].quote.latestPrice + '</span>';
+		markup += '</div>';
+		
+		$('.ticker_list').append(markup);
+		
+		let change = data[ticker].quote.change.toString();
+				
+		if (change.indexOf('-') != -1) { $('.ticker:last').addClass('down'); }
+		else { $('.ticker:last').addClass('up'); }
+	});
+	
+	var move_tickers = setInterval(function() {
+		activate_tickers();
+	}, 7500);
+}
+
+function activate_tickers() {
+	$('.ticker').each(function() {
+		var ticker_size = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+		
+		$(this).css('padding', '5px ' + ticker_size + '%' + '5px 5px');
+	});
+}
+
+function add_tickers() {
+	let tickers_to_add = $('.tickers_to_add').val().trim();
+	
+	if (tickers_to_add.length > 0) {
+		validate_ticker_input($('.tickers_to_add').val().trim());
+		
+		stock_up();
+	}
+}
+
+function validate_ticker_input(ticker_input) {
+	let temp_tickers = [];
+		
+	ticker_input = ticker_input.toUpperCase().replace(/[^A-Z,+]/g, '');
+		
+	if (ticker_input.length > 0) {
+		ticker_input = ticker_input.split(',');
+		tickers = tickers.concat(ticker_input);
+		
+		tickers.forEach(function(ticker) {
+			if (ticker.length > 0) {
+				temp_tickers.push(ticker);
+			}
+		});
+		
+		tickers = temp_tickers;
+	}
+}
+
+$(document).ready(function() {
+	stock_up();
+	
+	$('.add_tickers').click(function() {
+		add_tickers();
+	});
+	
+	$('.remove_ticker').click(function() {
+		$(this).parent().remove()
+	});
+});
