8eeafad1d319915d909914b7d423f54948c8b05f
Aaron Chapman <aaronchapman95@gmail.com>
2019-04-04 15:27:01 -0400

    2.2.6

--- stocka-extension/scripts/settings.js
+++ stocka-extension/scripts/settings.js
@@ -1,31 +1,24 @@
 // set up event listeners for settings containers
 function setup_settings() {
 	$('.open_settings').click(function() {
-		open_settings(settings);
+		open_settings();
 	});
 	
 	$('.save_settings').click(function() {
-		save_settings(settings);
+		save_settings();
 	});
 }
 
 // visually display settings container
-function open_settings(notes) {
+function open_settings() {
 	$('.open_settings').css('opacity', '0');
 	$('.settings_container').addClass('open').removeClass('closed');
 }
 
 // save selected settings
 function save_settings() {
-	settings = current_settings();
-	
 	chrome.storage.sync.set({'tickers': tickers, 'notes':notes, 'theme':current_theme, 'settings':settings}, function() {
 	  $('.settings_container').addClass('closed').removeClass('open');
 		$('.open_settings').css('opacity', '1');
 	});
-}
-
-// retrieve current selected settings (while saving)
-function current_settings() {
-	return {};
 }
