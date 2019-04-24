417647861a19948db238b42f0d4ff70524f7ca0c
Aaron Chapman <aaronchapman95@gmail.com>
2019-04-23 20:41:47 -0400

    open sourcing

--- stocka-extension/scripts/interface-utilities.js
+++ stocka-extension/scripts/interface-utilities.js
@@ -60,36 +60,6 @@ function setup_theme(theme_to_use) {
 	});
 }
 
-// put together additional interface elements for my lovely paying users
-function setup_upgraded_interface() {
-	add_settings();
-	add_chart();
-	add_saved_sets();
-}
-
-// add extra themes to settings menu
-function add_settings() {
-	$('.theme_option_container:last').after('<div class="theme_option_container"><button class="theme_option classic" data-theme-name="classic" aria-label="theme option: classic" tabindex="-1"></button><span class="theme_name">classic</span></div><div class="theme_option_container"><button class="theme_option midnight" data-theme-name="midnight" aria-label="theme option: midnight" tabindex="-1"></button><span class="theme_name">midnight</span></div>');
-	
-	$('.setting:last').after('<div class="setting market_performance_chart_type_setting"<span class="setting_category_title">chart:</span><hr><div class="graph_option_container"><button class="market_performance_chart_type bar" aria-label="market data chart type: bar graph" tabindex="-1">bar graph</button></div><div class="graph_option_container"><button class="market_performance_chart_type line" aria-label="market data chart type: line graph" tabindex="-1">line graph</button></div></div>');
-	
-	// and attach listeners to them
-	setup_settings_listeners();
-	setup_settings_states();
-}
-
-// add chart data to the detail view
-function add_chart() {
-	$('.ticker_detail_data').before('<div class="chart_container" role="presentation"><canvas id="change_chart" width="200" height="150"></canvas></div>');
-}
-
-function add_saved_sets() {
-	$('.options_header').prepend('<button class="saved_sets" aria-label="saved sets panel" aria-expanded="false">saved sets</button>');
-	
-	setup_saved_set_buttons();
-	setup_saved_sets();
-}
-
 // format larger numbers
 function number_with_commas(num) {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
