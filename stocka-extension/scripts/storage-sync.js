3bd752c1fe6ecad76ce36123c325a2fd3e4c700b
Aaron Chapman <aaronchapman95@gmail.com>
2019-05-07 20:07:21 -0400

    settings up sync file

---
+++ stocka-extension/scripts/storage-sync.js
@@ -0,0 +1,31 @@
+function load_saved_data() {
+	
+}
+
+function sync_symbol_set() {
+	
+}
+
+function sync_saved_sets() {
+	
+}
+
+function sync_notes() {
+	
+}
+
+function sync_settings() {
+	
+}
+
+function sync_sort_state() {
+	
+}
+
+function sync_theme() {
+	
+}
+
+function sync_chart_type() {
+	
+}
